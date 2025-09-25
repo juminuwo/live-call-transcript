c8c71345ebabaf724b69d9bb994abca165222752
Adrian Liu <adrianliu95@protonmail.com>
2025-09-25 17:37:07 +0100

    rename and move files

--- config.ps1
+++ config.ps1
@@ -1,3 +1,3 @@
 $pythonPath = "C:\Users\howis\miniconda3\envs\live_call_transcript_312\python.exe"
-$scriptPathCpu = "\\wsl.localhost\Ubuntu\home\howis\git\live_call_transcript\main_cpu.py"
-$scriptPathGpu = "\\wsl.localhost\Ubuntu\home\howis\git\live_call_transcript\main.py"
+$scriptPathCpu = "\\wsl.localhost\Ubuntu\home\howis\git\live_call_transcript\src\main_cpu.py"
+$scriptPathGpu = "\\wsl.localhost\Ubuntu\home\howis\git\live_call_transcript\src\main.py"
